4f407dfb876c138ff18d2256e30dd582499c65c7
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-05-07 15:19:52 +0200

    angular.js - splitting into sub controllers

--- django_web_server/static/js/app.js
+++ django_web_server/static/js/app.js
@@ -2,3 +2,13 @@ var appName = 'geoNodeTekApp';
 var appDependencies = ['GeoNodeTekControllers'];  
 
 var geoNodeTekApp = angular.module(appName, appDependencies);
+
+var geoNodeTekControllers = angular.module('GeoNodeTekControllers', []);
+
+geoNodeTekControllers.controller(
+	'GodController', ['$scope', '$http', '$timeout', GodController]
+);
+
+geoNodeTekControllers.controller(
+	'TrackListController', ['$scope', '$http', '$timeout', TrackListController]
+);
--- django_web_server/static/js/controllers/god.controller.js
+++ django_web_server/static/js/controllers/god.controller.js
@@ -1,6 +1,4 @@
-var geoNodeTekControllers = angular.module('GeoNodeTekControllers', []);
-
-function godController($scope, $http, $timeout) {
+function GodController($scope, $http, $timeout) {
 
 	$scope.globalDebug = function(raw_html) {
 		window.open('/echo/?' + raw_html, '_blank', '');
@@ -22,10 +20,6 @@ function godController($scope, $http, $timeout) {
 		$scope.$apply();
 	};
 
-	$scope.mapList = [];
-	$scope.mapSearchToken = '';
-	$scope.filteredMapList = [];
-	$scope.selectedMap = [];
 	$scope.mapInfoOverlayText = [];
 
 	$scope.mapIsLoadedAndActive = false;
@@ -202,65 +196,7 @@ function godController($scope, $http, $timeout) {
 		$scope.gfx.zoomIn();
 	}
 
-	// MAP LIST -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-	$scope.getMapList = function() {
-
-		var headers = { "Content-Type": "charset=utf-8" };
-		var request = { method: 'GET', url: "/maplist/", headers: headers };
-
-		var successFn = function(response) { 
-			$scope.loadMapList(response.maps); 
-		};
-		var errorFn = function(error){
-			$scope.globalDebug(error);
-		};
-
-		$http(request).success(successFn).error(errorFn);
-	};
-
-	$scope.loadMapList = function(mapList) {
-
-		$scope.mapList.length = 0;
-		for(var i in mapList){
-			$scope.mapList.push(mapList[i]);
-		}
-
-		$scope.filterMapList();
-	};
-
-	$scope.filterMapList = function() {		
-
-		matches = [];
-
-		// filter to matches
-		//
-		for(var i in $scope.mapList) {
-			if ($scope.mapList[i].name.toUpperCase().indexOf($scope.mapSearchToken.toUpperCase()) !== -1) {
-				matches.push($scope.mapList[i]);
-			}
-		}
-
-		// sort matches
-		//
-		matches.sort(function(a, b) { return a > b; });
-
-		// update filtered map list
-		//
-		$scope.filteredMapList.length = 0;
-		if (matches.length > 0) {			
-			for(var i in matches) {
-				$scope.filteredMapList.push(matches[i]);
-			}
-		}
-
-		// auto-select from matches
-		//
-		$scope.selectedMap.length = 0;
-		if (!(($scope.filteredMapList == undefined) || ($scope.filteredMapList.length == 0))) {
-			$scope.selectedMap.push($scope.filteredMapList[0]);
-		}
-	};
+	
 
 	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -441,10 +377,4 @@ function godController($scope, $http, $timeout) {
 	// START-UP
 
 	$scope.gfx = new Gfx('canvas', $scope.updateInfoText);
-
-	$scope.getMapList();
 }
-
-geoNodeTekControllers.controller(
-	'GodController', ['$scope', '$http', '$timeout', godController]
-);
