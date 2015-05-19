04220924dd442a824948dcc6e57d21fa9b658821
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-05-19 13:44:43 +0200

    view includes - infrastructure POC working

---
+++ django_web_server/static/js/angular-defer-bootstrap.js
@@ -0,0 +1,61 @@
+window.name = 'NG_DEFER_BOOTSTRAP! ' + window.name;
+
+function rawHTTP(method, url, onSuccess, onError)  {
+
+	xmlHttpReq = new XMLHttpRequest();
+
+  	xmlHttpReq.onreadystatechange = function() {
+
+        if (xmlHttpReq.readyState == XMLHttpRequest.DONE ) {
+
+           if ((xmlHttpReq.status == 200) || (xmlHttpReq.status == 304))
+               onSuccess(xmlHttpReq.responseText);
+           else {
+           		console.log('ERROR');
+           		console.log(xmlHttpReq.status);
+
+           		if (onError) {
+           			onError();
+           		}
+           }
+        }
+    };
+
+    xmlHttpReq.open(method, url, true);
+    xmlHttpReq.send();
+}
+
+window.onload = function () {
+
+	var attr = 'data-html-fragment';
+
+	var urlRoot = '/static/fragments/';
+
+	var next = function() {
+
+		var fragmentEls = document.querySelectorAll('[' + attr + ']');
+
+		if (fragmentEls.length) {
+
+			var element = fragmentEls[0];
+
+			var fragmentName = element.getAttribute(attr);
+
+			rawHTTP('GET', urlRoot + fragmentName + '.html',
+				function(html) {
+					element.innerHTML = html;
+					element.removeAttribute(attr);
+					console.log(html);
+
+					next();
+				}
+			);
+		}
+		else {
+			angular.resumeBootstrap();
+			window.name = window.name.replace('NG_DEFER_BOOTSTRAP! ', '');
+		}
+	};
+
+	next();
+};
