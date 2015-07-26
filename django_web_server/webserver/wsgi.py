f7522940e33b8d17a334cc1ca7c736a81446b303
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-07-26 09:11:11 +0200

    apache config consistent with repo structure

--- django_web_server/webserver/wsgi.py
+++ django_web_server/webserver/wsgi.py
@@ -5,7 +5,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 import sys
 
-path = '/var/www/gpxmapsnet/django'
+path = '/var/www/gpxmapsnet/django_web_server'
 if path not in sys.path:
     sys.path.insert(0, path)
 
