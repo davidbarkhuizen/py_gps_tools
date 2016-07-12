a7021487f5860f0486124ff99bf61f59722f2751
david barkhuizen <david.barkhuizen@gmail.com>
2016-07-12 20:06:50 +0200

    django version upgrade bugfix

--- django_web_server/webserver/wsgi.py
+++ django_web_server/webserver/wsgi.py
@@ -11,5 +11,5 @@ if path not in sys.path:
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webserver.settings")
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
